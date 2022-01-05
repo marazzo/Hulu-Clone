c38583248f741592eb8cb7b98eb0d6703c1fca12
Marianne Azzopardi <mer@Mariannes-MacBook-Pro.local>
2022-01-05 18:05:15 +0000

    added a dynamic fetched navbar woooo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
   return (
@@ -12,8 +14,8 @@ export default function Home() {
       </Head>
 
       <Header />
-      {/* Nav */}
-      {/* Results */}
+      <Nav />
+      <Results/>
     </div>
   );
 }
