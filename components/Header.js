34105ddc9ee8c9217008233d89f0c05f31b4c3ff
Marianne Azzopardi <mer@Mariannes-MacBook-Pro.local>
2022-01-05 16:20:31 +0000

    header completed

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+
       <Image
         className="object-contain"
         src="https://static.wikia.nocookie.net/logopedia/images/2/20/Hulu_2019.svg/revision/latest/scale-to-width-down/200?cb=20190508165749"
