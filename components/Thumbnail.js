bfff3d73b56c513b0584b5753d4a258b867ee1d6
Marianne Azzopardi <mer@Mariannes-MacBook-Pro.local>
2022-01-06 17:27:05 +0000

    completed

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import { forwardRef } from "react";
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+       className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={`${BASE_URL}${result.backdrop_path}`}
@@ -14,7 +18,7 @@ function Thumbnail({ result }) {
       <div>
         <p className="truncate max-w-md">{result.overview}</p>
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
-          {result.original_title}
+          {result.title || result.original_name}
         </h2>
         <p className="flex item-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type}`}•{result.release_date}•{" "}
@@ -23,6 +27,6 @@ function Thumbnail({ result }) {
       </div>
     </div>
   );
-}
+});
 
 export default Thumbnail;
