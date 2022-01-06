22ae285f4ffc24c7fff7ed47dd0f731fd5b93dd2
Marianne Azzopardi <mer@Mariannes-MacBook-Pro.local>
2022-01-06 17:17:45 +0000

    fetching from tmdb and thumbnails added

--- utils/requests.js
+++ utils/requests.js
@@ -7,42 +7,42 @@ export default {
   },
   fetchTopRated: {
     title: "Top Rated",
-    url: `movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   },
   fetchActionMovies: {
     title: "Action",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   },
   fetchComedyMovies: {
     title: "Comedy",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   },
   fetchHorrorMovies: {
     title: "Horror",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=27`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   },
   fetchRomanceMovies: {
     title: "Romance",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   },
-  fetchMysteryMovies: {
+  fetchMystery: {
     title: "Mystery",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
   },
-  fetchScifiMovies: {
+  fetchSciFi: {
     title: "Sci-Fi",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=878`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
   },
-  fetchWesternMovies: {
+  fetchWestern: {
     title: "Western",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=37`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
   },
-  fetchAnimationMovies: {
+  fetchAnimation: {
     title: "Animation",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=16`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
   },
-  fetchTVMovies: {
+  fetchTV: {
     title: "TV Movie",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=10770`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
   },
 };
